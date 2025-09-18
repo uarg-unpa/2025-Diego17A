e151dee9f02eac87b0c2b28d9a5861b2810e7175
Diego17A <161627119+Diego17A@users.noreply.github.com>
2025-09-18 18:45:35 -0300

    ACT 3 18/9/25

--- Clases/Pilas/PilaEnteros.java
+++ Clases/Pilas/PilaEnteros.java
@@ -9,31 +9,33 @@ public class PilaEnteros {
     public PilaEnteros() {
         elementos = new int[MAX]; // Crear el array
         // Completar: inicializar cima
-        elementos = new int [MAX];
         cima = -1;
     }
 
     // Verifica si la pila está vacía
     public boolean estaVacia() {
         // Completar: devolver true si cima indica que la pila está vacía
-            return false;
+            return (cima == -1);
     }
 
     // Verifica si la pila está llena
     public boolean estaLlena() {
         // Completar: devolver true si cima está en el límite del array
-        return false;
+        return (cima == MAX - 1);
     }
 
     // Agrega un elemento en la cima (push)
     public void meter(int elem) {
               // Completar: incrementar cima y asignar el elemento
-
+        cima ++;
+        elementos [cima] = elem;
     }
 
     // Saca y devuelve el elemento de la cima (pop)
     public int sacar() {
                   // Completar: guardar el elemento de la cima, decrementar cima y devolverlo
-
+        int aux = elementos [cima];
+        cima --;
+        return aux;
     }
 } 
