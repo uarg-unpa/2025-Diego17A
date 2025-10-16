1d52675e9bb0a3010dff7d7f95687fb4f12eeff8
Diego17A <161627119+Diego17A@users.noreply.github.com>
2025-10-16 13:56:52 -0300

    act 16/10/25

--- EjerElector/Elector.java
+++ EjerElector/Elector.java
@@ -1,5 +1,51 @@
 package EjerElector;
 
 public class Elector {
+    private String nombre;
+    private String apellido;
+    private String matricula;
+    private int clase;
+    private String domicilio;
+
+    public Elector (String nombre, String apellido, String matricula, int clase, String domicilio){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.matricula = matricula;
+        this.clase = clase;
+        this.domicilio = domicilio;
+    }
+    public void setnombre (String nombre){
+        this.nombre = nombre;
+    }
+    public String getnombre(){
+        return this.nombre;
+    }   
+    public void setapellido (String apellido){
+        this.apellido = apellido;
+    }
+    public String getapellido(){
+        return this.apellido;
+    }   
+    public void setmatriucla (String matricula){
+        this.matricula = matricula;
+    }
+    public String getmatricula(){
+        return this.matricula;
+    }
+    public void setdomicilio (String domicilio){
+        this.domicilio = domicilio;
+    }
+    public String getdomicilio(){
+        return this.domicilio;
+    }     
+    public void setclase (int clase){
+        this.clase = clase;
+    }
+    public int getclase(){
+        return this.clase;
+    }     
+    public String toString(){
+        return "["+this.nombre+"]["+this.apellido+"]["+this.matricula+"]["+this.domicilio+"]["+this.clase+"]";   
+    }
     
 }
--- EjerElector/TestElector.java
+++ EjerElector/TestElector.java
@@ -1,3 +1,4 @@
+
 //Crea una clase Elector con atributos:nombre // ejemplo Marta,apellido // ejemplo Saldía
 //matrícula // ejemplo 23493749,clase // 1956,domicilio // Libertad 1099
 //Incluye:constructor completo,métodos gets y sets,toString()
@@ -7,6 +8,29 @@
 
 package EjerElector;
 
+import java.util.Scanner;
+
 public class TestElector {
+    
+    public static void main (String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println ("Ingrese datos del elector 1 \nNombre:");
+        String nombre = scanner.nextLine();
+        System.out.println ("Apellido:");
+        String apellido = scanner.nextLine();
+        System.out.println ("matricula:");
+        String matricula = scanner.nextLine();
+        System.out.println ("clase:");
+        int clase = scanner.nextInt();
+        System.out.println ("domicilio:");
+        String domicilio = scanner.nextLine();
+
+        Elector E1 = new Elector(nombre,apellido,matricula,clase,domicilio);
+        
+
+
+    }
     
 }
