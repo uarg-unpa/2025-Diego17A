453593b2e3d5dc2f8302c44153f2c9c7161e98d1
Diego17A <161627119+Diego17A@users.noreply.github.com>
2025-11-27 17:29:59 -0300

    act
    weg

---
+++ Parciales/Parcial3/Ejercicio1.java
@@ -0,0 +1,31 @@
+package Parciales.Parcial3;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese cantidad de elementos:");
+        int n = scanner.nextInt();
+
+        int[] v = new int[n];
+
+        System.out.println("Ingrese los elementos:");
+        for (int i = 0; i < n; i++) {
+            v[i] = scanner.nextInt();
+        }
+        System.out.println("Negativos: " + contarNeg(v, 0));
+
+        scanner.close();
+    }
+    public static int contarNeg(int[] v, int i) {
+        if (i == v.length) return 0;
+
+        if (v[i] < 0)
+            return 1 + contarNeg(v, i + 1);
+        else
+            return contarNeg(v, i + 1);
+    }
+}
